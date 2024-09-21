5a9f61c54e781381cf400943388789aa0308bcbc
zomercado <97926050+zomercado@users.noreply.github.com>
2024-09-21 18:24:17 -0400

    made the keys in the packSizeOptions object as strings for precision, and moved js script inside of body at the end

--- solution-hw3/js/random-element.js
+++ solution-hw3/js/random-element.js
@@ -1,30 +1,30 @@
-// GOAL: update the cinnamon roll prices based on user selection of glazing and pack sizes//
+// GOAL: update prices based on user glazing + size selection//
 
-//create a dictionary (object) for the glazing and pack sizing //
+//create a dictionary (object) for the glazing and sizing//
 const glazingOptions = {
-  //each key is the name of a glazing option, and the value is the price adaptation//
+  //each key = name of a glazing option; value = price adaptation//
   "Keep original": 0.0,
   "Sugar milk": 0.0,
   "Vanilla milk": 0.5,
-  "Double chocolate": 1.5,
+  "Double chocolate": 1.5
 };
 
 const packSizeOptions = {
-  1: 1,
-  3: 3,
-  6: 5,
-  12: 10,
+  "1": 1,
+  "3": 3,
+  "6": 5,
+  "12": 10
 };
 
-//Use a base price for a cinnamon roll of $ 2.49.//
+//Use a base price for a cinnamon roll of $ 2.49//
 const basePrice = 2.49;
 
-//Populate the options of drop-down fields with these objects (separate functions for glaze and sizing)//
+//Populate (separately) the options of drop-down fields with these objects//
 function populateGlazingOptions() {
   const glazingSelect = document.getElementById("glazingOptions");
 
   //loop//
-  // const glazing is created by the loop to refer to each key in the object during iteration.//
+  // const glazing is created by the loop to refer to each key in the object during iteration//
   for (const glazing in glazingOptions) {
     const option = document.createElement("option"); //using JS to create options instead of hardcoding HTML//
     option.text = glazing;
@@ -57,13 +57,13 @@ function calculatePrice() {
     document.getElementById("packSizeOptions").value
   );
 
-  // Compute the price in the following manner: (basePrice + glazingPrice) * packPrice. //
+  // Compute the price: (basePrice + glazingPrice) * packPrice. //
   const finalPrice = (basePrice + selectedGlazingPrice) * selectedPackSizePrice;
 
-  //Update the price field on the Detail page to show the price of the current user selection.//
+  //Update price field on Detail page to show price of current user selection//
   document.getElementById(
     "price-detail-page"
-  ).innerText = `$ ${finalPrice.toFixed(2)}`; //from Stack Overflow - convert to string and round to 2 decimals//
+  ).innerText = `$ ${finalPrice.toFixed(2)}`; //Stack Overflow - round 2 decimals and convert to string//
 }
 
 document
