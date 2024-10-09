ab2840f87c8900cdbec77faa74445ab4a029798e
zomercado <97926050+zomercado@users.noreply.github.com>
2024-10-09 11:32:07 -0400

    I FINISHED!!!

---
+++ solution-hw5/js/app.js
@@ -0,0 +1,181 @@
+//GOAL: update prices based on user glazing + size selection//
+
+//Populate (separately) the options of drop-down fields with these objects//
+function populateGlazingOptions() {
+  const glazingSelect = document.getElementById("glazingOptions");
+
+  //loop//
+  //const glazing is created by the loop to refer to each key in the object during iteration//
+  for (const glazing in glazingOptions) {
+    const option = document.createElement("option"); //using JS to create options instead of hardcoding HTML//
+    option.text = glazing;
+    option.value = glazingOptions[glazing]; // Set the price adaptation as the option value//
+    glazingSelect.appendChild(option); //adds option to dropdown//
+  }
+}
+
+function populatePackSizeOptions() {
+  const packSizeSelect = document.getElementById("packSizeOptions");
+
+  //loop//
+  //const sizing is created by the loop to refer to each key in the object during iteration//
+
+  for (const sizing in packSizeOptions) {
+    const option = document.createElement("option");
+    option.text = sizing;
+    option.value = packSizeOptions[sizing];
+    packSizeSelect.appendChild(option);
+  }
+}
+
+//write a function to update pricing based on selection//
+//get elements by ID//
+function calculatePrice() {
+  const selectedGlazingPrice = parseFloat(
+    document.getElementById("glazingOptions").value
+  );
+  const selectedPackSizePrice = parseFloat(
+    document.getElementById("packSizeOptions").value
+  );
+
+  //Compute the price: (basePrice + glazingPrice) * packPrice.//
+  const finalPrice =
+    (rollBasePrice + selectedGlazingPrice) * selectedPackSizePrice;
+
+  //Update price field on Detail page to show price of current user selection//
+  document.getElementById(
+    "price-detail-page"
+  ).innerText = `$ ${finalPrice.toFixed(2)}`; //Stack Overflow - round 2 decimals and convert to string//
+}
+
+//HW 4 begins//
+
+//Parse the URL parameter and store the current roll type as a variable//
+const queryString = window.location.search;
+console.log(queryString);
+const params = new URLSearchParams(queryString);
+const rollType = params.get("roll");
+console.log(rollType);
+
+//Access the roll data from the rolls object (name, price, image path)//
+
+const selectedRoll = rolls[rollType];
+const rollBasePrice = selectedRoll.basePrice;
+const imageFile = selectedRoll.imageFile;
+
+//The Heading should be <Type> Cinnamon Roll//
+
+document.getElementById("roll-heading").innerText = `${rollType} Cinnamon Roll`;
+document.getElementById("detail-roll-image").src = `${imageFile}`;
+
+document
+  .getElementById("glazingOptions")
+  .addEventListener("change", calculatePrice);
+document
+  .getElementById("packSizeOptions")
+  .addEventListener("change", calculatePrice);
+
+//call functions!//
+populateGlazingOptions();
+populatePackSizeOptions();
+calculatePrice();
+
+//add to cart function//
+//create an empty cart array//
+let cart = [];
+function addToCart() {
+  const selectedGlazingValue = document.getElementById("glazingOptions").value;
+  const selectedPackSizeValue = document.getElementById("packSizeOptions").value;
+
+  //so that the console.log prints the key instead of the value - link from StackOverflow below//
+  //https://stackoverflow.com/questions/9907419/how-to-get-a-key-in-a-javascript-object-by-its-value//
+  const selectedGlazing = Object.keys(glazingOptions).find(
+    (key) => glazingOptions[key] == selectedGlazingValue
+  );
+  const selectedPackSize = Object.keys(packSizeOptions).find(
+    (key) => packSizeOptions[key] == selectedPackSizeValue
+  );
+
+  //cart class//
+  class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+      this.type = rollType;
+      this.glazing = rollGlazing;
+      this.size = packSize;
+      this.basePrice = basePrice;
+    }
+  }
+  // Add a new instance of Roll to the cart array to use//
+  const roll = new Roll(
+    rollType,
+    selectedGlazing,
+    selectedPackSize,
+    rollBasePrice
+  );
+  cart.push(roll); //add Roll instance to the array cart//
+  console.log(cart); //print cart to console//
+}
+
+//event listener//
+document.getElementById("addToCartBtn").addEventListener("click", addToCart);
+console.log("Roll Type: ", rollType);
+console.log("Selected Roll: ", selectedRoll);
+
+//HW 5 Start//
+// Do not hard-code the calculated price//
+
+const originalRoll = new Roll(
+  "Original",
+  "Sugar Milk",
+  1,
+  2.49
+);
+
+const walnutRoll = new Roll(
+  "Walnut",
+  "Vanilla Milk",
+  12,
+  39.90
+)
+
+const raisinRoll = new Roll(
+  "Raisin",
+  "Sugar Milk",
+  3,
+  8.97
+)
+
+const appleRoll = new Roll( 
+  "Apple",
+  "Original",
+  3,
+  10.47
+)
+
+cart.push(originalRoll);
+cart.push(walnutRoll);
+cart.push(raisinRoll);
+cart.push(appleRoll);
+
+function displayCartItem (Roll){
+  
+  const cartItemContainer = document.getElementByClass("cart-items");
+
+  const cartItemDiv = document.getElementByClass("cart-items");
+
+  // Add roll image
+  const rollImg = document.getElementById(`${imageFile}`);
+
+  // Add roll name
+
+  // Add glazing
+
+  // Add pack size
+
+  // Add item price (calculated)
+
+  // Add remove button
+  let template = document.getElementById("custom-paragraph");
+  let templateContent = template.content;
+  document.body.appendChild(templateContent);
+}
---
+++ solution-hw5/js/rollsData.js
@@ -0,0 +1,41 @@
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "../assets/products/original-cinnamon-roll.jpg" 
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "../assets/products/apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "../assets/products/raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "../assets/products/walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "../assets/products/double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "../assets/products/strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+const glazingOptions = {
+    "Keep original": 0.0,
+    "Sugar milk": 0.0,
+    "Vanilla milk": 0.5,
+    "Double chocolate": 1.5
+  };
+  
+  const packSizeOptions = {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10
+  };
+  
